76c5f984cc697e432b2c0e53495cd6bb867ad1ba
Rudra <Rudrayyaswamims@gmail.com>
2023-04-27 22:42:45 +0530

    Inputs

---
+++ 04.inputs.sh
@@ -0,0 +1,12 @@
+# 0-n , $*/$@ , $#
+
+echo First Argument $1 # First Argument
+
+echo Second Argument $2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+echo All Argument $*
+echo All Argument $@ # All Argument
+
+echo Number of Argument $# # Number of Arguments
