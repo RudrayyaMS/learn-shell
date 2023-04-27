f4ab72c8a29ccbc57f2682792cd661d2309b22ee
Rudra <Rudrayyaswamims@gmail.com>
2023-04-27 22:28:59 +0530

    variable

--- 03.variable.sh
+++ 03.variable.sh
@@ -1,2 +1,10 @@
 DATE=2023-04-27
 echo "hello hood morning , today date is $DATE"
+
+#Command Substitution
+DATE=${date}
+echo "Hello, Good Morning, today date is ${DATE}"
+
+# Airthmetic Substitution
+ADD=${(2+3)}
+echo "Addition of 2+3 is ${ADD}"
