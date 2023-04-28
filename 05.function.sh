bbc050dc500a55e43854297f80792a397eac82de
Rudra <Rudrayyaswamims@gmail.com>
2023-04-28 15:55:09 +0530

    updating learn-shell

--- 05.function.sh
+++ 05.function.sh
@@ -1,8 +1,11 @@
 #declare a function
 xyz() {
    echo hello from function
-
+  echo first argument - $1
+  echo second argument - $2
+  echo all argument - $*
+  echo No of argument - $#
 }
 
 ##calling a function
-xyz
+xyz 123 456
